99c1cc53442871faca9021fb4356d92222de0c1f
hashcott <duchanhstyle@gmail.com>
2022-04-13 00:59:10 +0700

    add model and template

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,32 @@
 from flask import current_app as app
 from flask import render_template
+from flask import request
+import numpy as np 
+import pandas as pd 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+import pickle 
+import json
+
+
+loaded_model = pickle.load(open('src/model/knnpickle_file.pkl', 'rb'))
+loaded_scaler = pickle.load(open('src/model/scaler_file.pkl', 'rb'))
 
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template(template_name_or_list="index.jinja2")
+
+@app.route("/analysis", methods=["POST"])
+def analysis():
+    data = request.get_json()
+    with open('./data.json', 'w') as f:
+        json.dump(data, f)
+    df = pd.read_json('./data.json')
+    df.head()
+    cols = df.columns
+
+    X_test = loaded_scaler.transform(df)
+    X_test = pd.DataFrame(X_test, columns=[cols])
+    result = loaded_model.predict(X_test)
+    return str(result[0])
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,3 +1,79 @@
-document.onreadystatechange = function () {
-  document.getElementById("footer-desc").innerHTML = "Simple JS DOM Manipulation!"
-}
+$(document).ready(() => {
+  $("#formIndex").on("submit", async (e) => {
+    e.preventDefault();
+    let data = [
+      {
+        Sex_M: 0,
+        ChestPainType_ATA: 0,
+        ChestPainType_NAP: 0,
+        ChestPainType_TA: 0,
+        RestingECG_Normal: 0,
+        RestingECG_ST: 0,
+        ExerciseAngina: 0,
+        ST_Slope_Flat: 0,
+        ST_Slope_Up: 0,
+        Age: 0,
+        RestingBP: 0,
+        Cholesterol: 0,
+        FastingBS: 0,
+        MaxHR: 0,
+        Oldpeak: 0,
+      },
+    ];
+    data[0].Sex_M = parseInt($("input[name='sex']:checked").val());
+    data[0].FastingBS = parseInt($("input[name='fbs']:checked").val());
+    const cptRaw = parseInt($("#cpt").val());
+    switch (cptRaw) {
+      case 1:
+        data[0].ChestPainType_ATA = 1;
+        break;
+      case 2:
+        data[0].ChestPainType_NAP = 1;
+        break;
+      case 4:
+        data[0].ChestPainType_TA = 1;
+        break;
+      default:
+        break;
+    }
+    const recgRaw = parseInt($("#recg").val());
+    switch (recgRaw) {
+      case 1:
+        data[0].RestingECG_Normal = 1;
+        break;
+      case 2:
+        data[0].RestingECG_ST = 1;
+        break;
+      default:
+        break;
+    }
+    data[0].ExerciseAngina = parseInt($("input[name='ea']:checked").val());
+    const stlRaw = parseInt($("#stl").val());
+    switch (stlRaw) {
+      case 1:
+        data[0].ST_Slope_Up = 1;
+        break;
+      case 2:
+        data[0].ST_Slope_Flat = 1;
+        break;
+      default:
+        break;
+    }
+    data[0].Age = parseInt($("#ageInput").val());
+    data[0].RestingBP = parseInt($("#restingBPInput").val());
+    data[0].Cholesterol = parseInt($("#cholesterol").val());
+    data[0].MaxHR = parseInt($("#maxHr").val());
+    data[0].Oldpeak = parseFloat($("#oldPeak").val());
+
+    const res = await fetch("/analysis", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await res.json();
+    console.log(resData);
+  });
+});
